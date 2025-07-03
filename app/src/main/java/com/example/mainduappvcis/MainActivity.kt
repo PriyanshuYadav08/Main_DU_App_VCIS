f615f48e027144399c6e5558085dcbd47c2601f8
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-07-03 12:36:10 +0530

    Initial UI updates and toolbar integration.

--- app/src/main/java/com/example/mainduappvcis/MainActivity.kt
+++ app/src/main/java/com/example/mainduappvcis/MainActivity.kt
@@ -1,20 +1,55 @@
 package com.example.mainduappvcis
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "University Of Delhi"
+        for (i in 0 until toolbar.childCount) {
+            val view = toolbar.getChildAt(i)
+            if (view is TextView && view.text == toolbar.title) {
+                val typeface = ResourcesCompat.getFont(this, R.font.pjs_bold)
+                view.typeface = typeface
+                view.textSize = 15f
+                view.setTextColor(resources.getColor(R.color.white))
+                break
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.header_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.option1 -> {
+                Toast.makeText(this, "Option 1 clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.option2 -> {
+                Toast.makeText(this, "Option 2 clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.option3 -> {
+                Toast.makeText(this, "Option 3 clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
