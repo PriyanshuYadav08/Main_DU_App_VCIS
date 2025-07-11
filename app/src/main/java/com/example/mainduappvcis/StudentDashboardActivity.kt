1ae4314de2c34e15f0f3c6d40d310d26b96c25bd
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-07-11 11:30:27 +0530

    UI integration completed. All links working.

--- app/src/main/java/com/example/mainduappvcis/StudentDashboardActivity.kt
+++ app/src/main/java/com/example/mainduappvcis/StudentDashboardActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mainduappvcis
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,37 @@ class StudentDashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_student_dashboard)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val calenderTimetable = findViewById<Button>(R.id.calender_timetable)
+        val assignmentExam = findViewById<Button>(R.id.assign_exam)
+        val digitalCert = findViewById<Button>(R.id.digitalcert_idcards)
+        val resultGrades = findViewById<Button>(R.id.result_grades_btn)
+        val feedbackGrievance = findViewById<Button>(R.id.feedback_grievance_btn)
+
+        calenderTimetable.setOnClickListener {
+            openWebsite("https://www.du.ac.in/index.php?page=academic-calendar")
+        }
+        assignmentExam.setOnClickListener {
+            openWebsite("https://exam.du.ac.in/index.php")
+        }
+        digitalCert.setOnClickListener {
+            openWebsite("https://digicerti.du.ac.in/")
+        }
+        resultGrades.setOnClickListener {
+            openWebsite("https://slc.uod.ac.in/index.php/site/login")
+        }
+        feedbackGrievance.setOnClickListener {
+            openWebsite("https://app.uod.ac.in/sgr/index.php/form/create")
+        }
+    }
+
+    private fun openWebsite(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "No browser found to open this link", Toast.LENGTH_SHORT).show()
         }
     }
 }
